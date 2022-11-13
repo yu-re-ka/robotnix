fef5f8fcd48e817dafa21dacab4f0a5a24c0e327
Tomek Mańko <tomek.manko@railgun-solutions.com>
2022-11-13 13:25:52 +0100

    Add slider to `source.includeGroups` for raviole

--- modules/pixel/default.nix
+++ modules/pixel/default.nix
@@ -73,7 +73,8 @@ mkMerge [
     # Exclude all devices by default
     source.excludeGroups = mkDefault (lib.attrNames deviceMap ++ lib.mapAttrsToList (name: device: device.family) deviceMap ++ [ "slider" ]);
     # But include names related to our device
-    source.includeGroups = mkDefault [ config.device config.deviceFamily ];
+    source.includeGroups = mkDefault ([ config.device config.deviceFamily  ]
+                                        ++ lib.optional (config.deviceFamily == "raviole") "slider");
 
     signing.avb.enable = mkDefault true;
   })
