70e451889863d9119ffa8e4162da428bfaf2ead6
Tomek Mańko <tomek.manko@railgun-solutions.com>
2022-11-16 22:15:07 +0100

    Virtualisation readonly paths

--- modules/13/default.nix
+++ modules/13/default.nix
@@ -11,6 +11,9 @@ mkIf (config.androidVersion == 13) {
     ./build_make/0001-Readonly-source-fix.patch
     #./build_make/0003-Add-option-to-include-prebuilt-images-when-signing-t.patch # Moved temporarily to modules/pixel/driver-binaries.nix
   ];
+  source.dirs."packages/modules/Virtualization".patches = [
+    ./packages_modules_Virtualization/0001-Readonly-source-fix.patch
+  ];
 
   #kernel.clangVersion = mkDefault "r370808";
 }
