9262be7ca5b79f11fed0ffdceaf711b63059bc7b
Yureka <yuka@yuka.dev>
2022-11-16 18:06:51 +0100

    add new readonly source patch

--- modules/13/default.nix
+++ modules/13/default.nix
@@ -11,6 +11,9 @@ mkIf (config.androidVersion == 13) {
     ./build_make/0001-Readonly-source-fix.patch
     #./build_make/0003-Add-option-to-include-prebuilt-images-when-signing-t.patch # Moved temporarily to modules/pixel/driver-binaries.nix
   ];
+  source.dirs."packages/modules/Virtualization".patches = [
+    ./virtualization-readonly-source.diff
+  ];
 
   #kernel.clangVersion = mkDefault "r370808";
 }
